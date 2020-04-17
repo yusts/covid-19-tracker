9d527c68f3fd0cbd5044cc1ccbf9bccc84db4d7a
Yuriy Stasiv <stasivyura@gmail.com>
2020-04-17 14:35:12 +0200

    Updates

---
+++ precache-manifest.60ca43dfbd217bad38afb35865fe8b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87db7c2f6c8eb7008e97517df80549ae",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "eda42e55a71ebde7ee2c",
+    "url": "/covid-19-tracker/static/css/main.f1621042.chunk.css"
+  },
+  {
+    "revision": "ba0a7acfa916c9eb1e73",
+    "url": "/covid-19-tracker/static/js/2.74cef45c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.74cef45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda42e55a71ebde7ee2c",
+    "url": "/covid-19-tracker/static/js/main.9768c71b.chunk.js"
+  },
+  {
+    "revision": "131a3f8a707da252ef4a",
+    "url": "/covid-19-tracker/static/js/runtime-main.3c8d6362.js"
+  }
+]);
